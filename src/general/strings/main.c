5cde7fa4ba9c4b7f921e5afb1ed53169c77789cd
0x4248 <60709927+0x4248@users.noreply.github.com>
2024-04-11 11:55:47 +0100

    Username update
    Signed-off-by: 0x4248 <60709927+0x4248@users.noreply.github.com>

--- src/general/strings/main.c
+++ src/general/strings/main.c
@@ -1,9 +1,9 @@
 /* Compiled C
  * A collection of C programs that are compiled to assembly 
  * code for understanding how C and assembly work.
- * GitHub: https://wwww.github.com/lewisevans2007/Compiled_C
+ * GitHub: https://wwww.github.com/0x4248/Compiled_C
  * Licence: GNU General Public Licence v3.0
- * By: Lewis Evans
+ * By: 0x4248
  * 
  * This program defines strings.
 */
